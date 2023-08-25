8353f4a04e96b732ffc634ef31c12c70266ae5b7
Anderson Assunção <andersnn7@gmail.com>
2023-08-25 09:55:32 -0500

    update

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 
-
+app.use(express.static('public'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false}))
 
